9aa3e9fba14ea15fad5505602af0dd6136014232
Daniela Malta <danimaltoc@outlook.com>
2026-09-15 13:09:59 -0300

    Fix indentation and formatting in main.js

--- Zyphra/js/main.js
+++ Zyphra/js/main.js
@@ -1,5 +1,5 @@
 
-         document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     const menuBtn = document.getElementById("menuBtn");
     const nav = document.getElementById("nav");
